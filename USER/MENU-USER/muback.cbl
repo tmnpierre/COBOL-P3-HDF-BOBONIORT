a10eeacb5225252299b0820c83b0b02574d09a2e
tmnpierre <tomyn.pierre@gmail.com>
2024-06-05 11:09:52 +0200

    feat(validation): add muback subprogram for user input validation
    Added a new COBOL subprogram 'muback' to handle validation of user input, ensuring the user presses 'O' followed by Enter. Updated main program to call this subprogram in each section to validate user actions.

--- USER/MENU-USER/muback.cbl
+++ USER/MENU-USER/muback.cbl
@@ -1,3 +1,35 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. muback.
+       AUTHOR. Pierre.
+
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
        
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01  SC-MENU-RETURN PIC X(01).
+       01  VALID-CHOICE PIC X(01) VALUE 'O'.
+       
+       PROCEDURE DIVISION.
+       0000-START-VALIDATE-CHOICE.
+           PERFORM 1000-DISPLAY-VALIDATION-MESSAGE.
+           PERFORM 2000-ACCEPT-AND-VALIDATE.
+           PERFORM 9000-END-VALIDATE-CHOICE.
+           EXIT.
+       
+       1000-DISPLAY-VALIDATION-MESSAGE.
+           DISPLAY "Appuyez sur O puis Entree pour valider".
+           EXIT.
+       
+       2000-ACCEPT-AND-VALIDATE.
+           ACCEPT SC-MENU-RETURN.
+           MOVE FUNCTION UPPER-CASE(SC-MENU-RETURN) TO SC-MENU-RETURN.
+           IF SC-MENU-RETURN NOT = VALID-CHOICE
+               DISPLAY "CHOIX INVALIDE, ESSAYEZ ENCORE."
+               PERFORM 2000-ACCEPT-AND-VALIDATE
+           END-IF.
+           EXIT.
+       
+       9000-END-VALIDATE-CHOICE.
+           DISPLAY "Choix valide.".
+           EXIT.
